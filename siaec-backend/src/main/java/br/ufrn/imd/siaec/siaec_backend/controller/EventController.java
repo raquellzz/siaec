3a064898dcae4d259fc8c28ca0e487bd97e3f30d
Nicole <cnogueira.nicole@gmail.com>
2025-11-08 14:17:23 -0300

    feat: crud de events

---
+++ siaec-backend/src/main/java/br/ufrn/imd/siaec/siaec_backend/controller/EventController.java
@@ -0,0 +1,58 @@
+package br.ufrn.imd.siaec.siaec_backend.controller;
+
+import br.ufrn.imd.siaec.siaec_backend.model.Event;
+import br.ufrn.imd.siaec.siaec_backend.service.EventService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/events")
+public class EventController {
+    @Autowired
+    private EventService eventService;
+
+    @PostMapping
+    public ResponseEntity<Event> createEvent(@RequestBody Event event) {
+        return ResponseEntity.ok(eventService.createEvent(event));
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<Event>> getEvents(
+            @RequestParam(required = false) String name,
+            @PageableDefault(size = 10, page = 0) Pageable pageable) {
+
+        Page<Event> events = eventService.findEvents(name, pageable);
+        return ResponseEntity.ok(events);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Event> getEventById(@PathVariable String id) {
+        return eventService.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Event> updateEvent(@PathVariable String id, @RequestBody Event eventDetails) {
+        try {
+            Event updatedEvent = eventService.updateEvent(id, eventDetails);
+            return ResponseEntity.ok(updatedEvent);
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteEvent(@PathVariable String id) {
+        try {
+            eventService.deleteEvent(id);
+            return ResponseEntity.noContent().build();
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
