2b0f834873cc4e07562fb2ba6a86b034f4001017
Fabiana Pereira <fabianaduarte980@gmail.com>
2025-12-06 21:53:35 -0300

    fix: header logo

--- siaec-frontend/src/components/Header/Header.jsx
+++ siaec-frontend/src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import './Header.css';
-import logo from '../../assets/logo.png';
+import logo from '../../assets/logo-header.png';
 import { Link, useNavigate } from 'react-router-dom';
 import { useAuth } from '../../hooks/useAuth';
 import { FaUserCircle, FaShoppingCart } from 'react-icons/fa';
