e349835af9b69a42dc49306a17126c6c99b4bf4d
Fabiana Pereira <fabianaduarte980@gmail.com>
2025-12-10 01:06:21 -0300

    fix: corrige input

--- siaec-frontend/src/components/Input/index.jsx
+++ siaec-frontend/src/components/Input/index.jsx
@@ -26,7 +26,7 @@ export default function Input({
       error={error}
       multiline={multiline}
       rows={rows}
-      slotProps={{ inputLabel: { shrink: type === 'date' ? true : true } }}
+      slotProps={{ inputLabel: { shrink: type === 'date' ? true : undefined } }}
       {...otherProps}
     />
   );
